13e8ee735649fd9fd16dff6ab5a6eaec63528e60
Emily <oneill.emily@gmail.com>
2023-06-07 11:15:28 -0700

    Add detail view to router

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import LoginView from '../views/LoginView.vue';
 import MenuView from '../views/MenuView.vue';
 import RewardsView from '../views/RewardsView.vue';
 import SignupView from '../views/SignupView.vue';
+import DetailView from '../views/DetailsView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
 			path: '/signup',
 			name: 'signup',
 			component: SignupView,
+		},
+		{
+			path: '/menu/:slug',
+			name: 'detail',
+			component: DetailView
 		}
 		// {
 		// 	path: '/about',
