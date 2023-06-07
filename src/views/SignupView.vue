bf33bbab1a681f8a185c9f9881abe2a4be1ea829
Emily <oneill.emily@gmail.com>
2023-06-07 11:13:47 -0700

    Include sign up form component in sign up view

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+	import {ref, reactive} from 'vue';
+	import SignupForm from '@/components/SignupForm.vue';
+
+</script>
 
 <template>
-	<h1>Sign up</h1>
+	<section class="signup-header view-header">
+		<div class="inner-column">
+			<h1 class="heyyou-voice">Sign up</h1>
+		</div>
+	</section>
+
+	<section class="signup">
+		<div class="inner-column">
+			<SignupForm />
+		</div>
+	</section>
 </template>
 
-<style></style>
+<style>
+	.signup-header {
+		background-color: var(--tertiary);
+	}
+</style>
