554ef389f67a98ed9114c9a5488d98caa6c8b30e
Emily <oneill.emily@gmail.com>
2023-06-07 11:14:12 -0700

    Add header and grid to menu view

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+	import MenuGrid from '@/components/MenuGrid.vue';
+	import { useMenusStore } from '@/stores/menus';
+	import { useRouter, useRoute, RouterLink } from 'vue-router';
+
+	const menus = useMenusStore();
+</script>
 
 <template>
-	<h1>Menu</h1>
+
+	<section class="menu-header view-header">
+		<div class="inner-column">
+			<h1 class="heyyou-voice">Menu</h1>
+		</div>
+	</section>
+
+
+	<div class="inner-column">
+		<MenuGrid />	
+	</div>
+
+
 </template>
 
-<style></style>
+<style scoped>
+	.menu-header {
+		background-color: var(--main);
+	}
+
+</style>
+
+
