be2664e5d24baa796b6cf505122e3a1dc08fec3e
Emily <oneill.emily@gmail.com>
2023-06-07 11:15:17 -0700

    Add password validation ref to interface store

--- src/stores/interface.js
+++ src/stores/interface.js
@@ -4,6 +4,8 @@ import { defineStore } from 'pinia';
 export const useInterfaceStore = defineStore('interface', function () {
 	const mainMenuOpen = ref(false);
 
+	const pwValidation = ref(false);
+
 	function toggleMenu() {
 		mainMenuOpen.value = !mainMenuOpen.value;
 	}
@@ -16,5 +18,22 @@ export const useInterfaceStore = defineStore('interface', function () {
 		}
 	});
 
-	return { mainMenuOpen, toggleMenu, menuClass };
+	return { mainMenuOpen, toggleMenu, menuClass, pwValidation };
 });
+
+// import { reactive } from 'vue';
+// import { defineStore } from 'pinia';
+
+// export const useMonstersStore = defineStore('monsters', () => {
+// 	const list = reactive([]);
+
+// 	function add(monster) {
+// 		list.push(monster);
+// 	}
+
+// 	function clear() {
+// 		localStorage.clear();
+// 	}
+
+// 	return { list, add, clear };
+// });
