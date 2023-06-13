af3180e569a398dfb73720bd698add12e5d0bf55
Emily <oneill.emily@gmail.com>
2023-06-13 07:46:24 -0700

    Change naming of userDoc to form for consistency

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -13,137 +13,36 @@
 
 	const ui = useInterfaceStore();
 	const users = useUsersStore();
-
-
-
-	// function makeUser() {
-	// 	getAuth()
-	// 		.createUser({
-	// 			email: userDoc.email,
-	// 			displayName: userDoc.name
-	// 		})
-	// 		.then((userRecord) => {
-	// 			console.log(userRecord.uid);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log(error);
-	// 		});
-	// }
-
-
 	
-	const userDoc = reactive({
+	const form = reactive({
 		email: "",
 		name: "",
 		password: "",
 		passwordConf: "",
 	});
 
-	const docData = reactive({
-		displayName: userDoc.name,
-		email: userDoc.email,
-		// uid: users.current.uid
-	})
-	
-
-	function addUser() {
-	
-			addDoc(collection(users.db, 'users'), {
-				displayName: userDoc.name,
-				email: userDoc.email,
-				uid: users.current.uid
-			});
-		// getAuth()
-		// 	.getUser(users.current.uid)
-		// 	.then((userRecord) => {
-		// 		console.log(userRecord);
-
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log('Error fetching user data:', error);
-		// 	});
-
-		// addDoc(collection(users.db, 'users'), userRecord);
-		// setDoc(doc(users.db, 'users', users.current.uid), userRecord);
-	}
-	// function addUser() {
-
-	// 	setDoc(doc(users.db, "users", users.current.uid) {
-	// 		displayName: userDoc.name,
-	// 		email: userDoc.email,
-	// 		uid: users.current.uid
-	// 	});
-		
-		
-	
-			// addDoc(collection(users.db, 'users'), {
-			// 	displayName: userDoc.name,
-			// 	email: userDoc.email,
-			// 	uid: uid
-			// });
-		
-	// }
-
-	function updateInfo() {
-					getAuth()
-  				.updateUser(users.authUser.uid, {
-    				email: 'modifiedUser@example.com',
-    				phoneNumber: '+11234567890',
-   				emailVerified: true,
-					password: 'newPassword',
-					displayName: 'Jane Doe',
-					photoURL: 'http://www.example.com/12345678/photo.png',
-					disabled: true,
-				})
-  				.then((userRecord) => {
-			    // See the UserRecord reference doc for the contents of userRecord.
-			    console.log('Successfully updated user', userRecord.toJSON());
-				})
-				.catch((error) => {
-    				console.log('Error updating user:', error);
-				});
-	}
-
-	function createUserDoc() {
-		setDoc(doc(users.db, "users", users.current.uid), {
-			displayName: userDoc.name,
-			email: userDoc.email,
-			uid: users.current.uid
-		});
-
-		console.log("profile updated");
-	}
-
-	const user = reactive({
-		displayName: "",
-		email: ""
-	});
-
 	function passConfirm() {
-		console.log(userDoc.password);
-		console.log(userDoc.passwordConf);
+		console.log(form.password);
+		console.log(form.passwordConf);
 		console.log(ui.pwValidation);
 
-		if (userDoc.password !== userDoc.passwordConf) {
+		if (form.password !== form.passwordConf) {
 			console.log ('Your password does not match. Please double check.');
 
 			ui.pwValidation = true;
 
-		} else if (userDoc.password === userDoc.passwordConf) {
+		} else if (form.password === form.passwordConf) {
 
 			console.log('Your passwords match');
 			ui.pwValidation = false;
-			users.signUp(userDoc.name, userDoc.email, userDoc.password);
-			
-			router.push('/account');
-	}
-	
+			users.signUp(form);
+		}
 	}
 
 	function clear() {
-		userDoc.email = '',
-		userDoc.password = '',
-		userDoc.passwordConf = ''
+		form.email = '',
+		form.password = '',
+		form.passwordConf = ''
 
 	}
 	// on successful sign up, send user to main page with validation message? 
@@ -155,33 +54,19 @@
 	<form @submit.prevent="passConfirm" id="signup-form" autocomplete="off">
 		<div class="form-field">
 			<label class="reading-voice" for="name">Your name</label>
-			<input v-model="userDoc.name" class="reading-voice" id="name" type="text" required>
+			<input v-model="form.name" class="reading-voice" id="name" type="text" required>
 		</div>
 		<div class="form-field">
 			<label class="reading-voice" for="email">Email address</label>
-			<input v-model="userDoc.email" class="reading-voice" id="email" type="email" required>
+			<input v-model="form.email" class="reading-voice" id="email" type="email" required>
 		</div>
-
-	
-<!-- 		<div class="form-field">
-			<label class="reading-voice" for="signupFirst">First name</label>
-			<input v-model="user.firstName" class="reading-voice" id="signupFirst" type="text" required>
-		</div>
-		<div class="form-field">
-			<label class="reading-voice" for="signupLast">Last name</label>
-			<input v-model="user.lastName" class="reading-voice" id="signupLast" type="text" required>
-		</div>
-		<div class="form-field">
-			<label class="reading-voice" for="password">Birthday</label>
-			<input v-model="user.birthday" class="reading-voice" id="signupBirth" type="date">
-		</div> -->
 		<div class="form-field">
 			<label class="reading-voice" for="signupPass">Password</label>
-			<input v-model="userDoc.password" class="reading-voice" id="signupPass" type="password" required>
+			<input v-model="form.password" class="reading-voice" id="signupPass" type="password" required>
 		</div>
 		<div class="form-field">
 			<label class="reading-voice" for="signupLast">Confirm password</label>
-			<input v-model="userDoc.passwordConf" class="reading-voice" id="signupPassConf" type="password" required>
+			<input v-model="form.passwordConf" class="reading-voice" id="signupPassConf" type="password" required>
 		</div>
 		<p v-if="ui.pwValidation" class="reading-voice">Your password does not match. Please double check.</p>
 
