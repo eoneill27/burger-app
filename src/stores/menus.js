92fe00d5f549b2585b7c640b3356961f541fa479
Emily <oneill.emily@gmail.com>
2023-06-16 07:04:52 -0700

    Add current user to menus store

--- src/stores/menus.js
+++ src/stores/menus.js
@@ -1,15 +1,23 @@
-import { reactive, computed } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import { defineStore } from 'pinia';
 
 
-import { collection, doc, getDocs } from 'firebase/firestore';
-import { useFirestore, useCollection } from 'vuefire';
+import { collection, doc, getDocs, setDoc, addDoc, updateDoc } from 'firebase/firestore';
+import { useFirestore, useCollection, useCurrentUser } from 'vuefire';
+
+
+
 
 export const useMenusStore = defineStore('menus', function () {
 
 	const db = useFirestore();
+	const current = useCurrentUser();
 
 	const menusCollection = useCollection(collection(db, 'food'));
+
+
+
+
 
 	return { db, menusCollection };
 });
