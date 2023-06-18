df8c47e288d393a692b105c63b9825a219ea3e2c
Emily <oneill.emily@gmail.com>
2023-06-17 22:14:11 -0700

    Add addToCart function to orders store

--- src/stores/orders.js
+++ src/stores/orders.js
@@ -3,18 +3,42 @@ import { defineStore } from 'pinia';
 
 
 import { collection, doc, getDocs, setDoc, addDoc, updateDoc } from 'firebase/firestore';
-import { useFirestore, useCollection, useCurrentUser } from 'vuefire';
+import { useFirestore, useCollection, useCurrentUser, getCurrentUser } from 'vuefire';
 
 export const useOrdersStore = defineStore('orders', () => {
 
 	const db = useFirestore();
 	const current = useCurrentUser();
 
-	// const currentOrder = useCollection(collection(db, 'users', current.uid, 'carts', 'cart1', 'items' ));
 
-	// const order = currentOrder.value;
+	async function addToCart(name, ingredients, specialInstr, quantity, price) {
+		
+		// await getCurrentUser();
+
+		const subtotal = Number(price * quantity);
+		await getCurrentUser();
+		if (specialInstr) {
+			specialInstr == specialInstr;
+		} else {
+			specialInstr = '';
+		}
+
+		await addDoc(collection(db, "users", current.uid, "carts", "cart1", "items"), {
+					name: name,
+					ingredients: ingredients,
+					specialInstr: specialInstr,
+					subtotal: subtotal,
+					quantity: quantity
+		});
+
+		if (quantity > 1) {
+			alert(`${quantity} ${name}s were added to your cart`);
+		} else {
+			alert(`1 ${name} was added to your cart`);		
+		}
+	}
 
 
 
-	return {  }
+	return { addToCart }
 });
