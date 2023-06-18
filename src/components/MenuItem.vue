986f3bfcc58420617e4aa95b8ef1b4db5c64f3fd
Emily <oneill.emily@gmail.com>
2023-06-18 08:22:04 -0700

    Add menu details modal

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -31,6 +31,7 @@
 	const buttonSelect = ref('Select');
 
 	function toggleDetails() {
+		clearOrder(specialInstr, quantity);
 		detailsOpen.value = !detailsOpen.value;
 	}
 
@@ -43,6 +44,12 @@
 			return 'details-closed';
 		}
 	})
+
+	function clearOrder(specialInstr, quantity) {
+		specialInstr = '';
+		quantity = 0;
+	
+	}
 
 	async function addToCart(name, ingredients, specialInstr, quantity, price) {
 		
@@ -69,39 +76,51 @@
 		} else {
 			alert(`1 ${name} was added to your cart`);		
 		}
+
+		// clearOrder(specialInstr, quantity);
+		// await clearOrder(specialInstr, quantity);
+		toggleDetails();
+		
 	}
 
 
 </script>
 <template class="menuItem-grid">
 
-	<button @click="toggleDetails" type="button" class="reading-voice">{{buttonSelect}}</button>
-
+	<button @click="toggleDetails" type="button" class="reading-voice">Select</button>
+	<!-- <div class="details-modal"> -->
 	<div :class="detailsClass" class="item-details">
-		<fieldset >
-			{{ingredients}}
-			<legend class="reading-voice">Customize your order:</legend>
-			<div v-for="(ingredient) in item.ingredients" >
-				<label class="reading-voice">{{ingredient}}</label>
-				<input type="checkbox" :value="ingredient" v-model="ingredients" class="reading-voice">
+		<div class="modal-content">
+		<form id="customizeForm">
+			<button @click="toggleDetails">Close</button>
+			<fieldset >
+			<!-- {{ingredients}} -->
+				<legend class="info-voice">Customize your order:</legend>
+				<div v-for="(ingredient) in item.ingredients" >
+					<label class="reading-voice">{{ingredient}}</label>
+					<input type="checkbox" :value="ingredient" v-model="ingredients" class="reading-voice">
+				</div>
+			</fieldset>
+
+			<div class="form-field" id="instructions">
+				<label for="specialInstr" class="info-voice">Special instructions:</label>
+				<textarea  v-model="item.specialInstr" name="" id="specialInstr" cols="25" rows="10" class="reading-voice"></textarea>			
 			</div>
-		</fieldset>
 
-		<div class="form-field" id="instructions">
-			<label for="specialInstr" class="reading-voice">Special instructions:</label>
-			<textarea  v-model="item.specialInstr" name="" id="specialInstr" cols="25" rows="10" class="reading-voice"></textarea>			
-		</div>
+			<p class="info-voice">Price: ${{item.price}}</p>
 
-		<p class="reading-voice">Price: ${{item.price}}</p>
+			<div class="form-field" id="quantity">
+				<label for="itemQuant" class="info-voice">Quantity:</label>
+				<input v-model="item.quant" id="itemQuant" type="number" min='1' class="reading-voice">
+			</div>
 
-		<div class="form-field" id="quantity">
-			<label for="itemQuant" class="reading-voice">Quantity:</label>
-			<input v-model="item.quant" id="itemQuant" type="number" >
-		</div>
+			<button @click="addToCart(item.name, ingredients, item.specialInstr, item.quant, item.price)"  class="reading-voice" id="addButton" type="button">Add to cart
+			</button>
+		</form>
 
-		<button @click="orders.addToCart(item.name, ingredients, item.specialInstr, item.quant, item.price)"  class="reading-voice" id="addButton" type="button">Add to cart
-		</button>
 	</div>
+	</div>
+	<!-- </div> -->
 
 </template>
 <style>
@@ -143,6 +162,27 @@
 
 	.details-open {
 		display: block;
+		position: fixed;
+		z-index: 1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: rgb(0,0,0);
+		background-color: rgba(0,0,0,0.4);
+	}
+
+	.modal-content {
+	  background-color: #fefefe;
+	  margin: 15% auto; 
+	  padding: 20px;
+	  border: 1px solid #888;
+	  width: 60%; /* Could be more or less, depending on screen size */
+	}
+
+	.menuItem-card .details-open {
+
 	}
 
 	#instructions {
@@ -154,7 +194,7 @@
 		padding: 10px 0;
 	}
 	#itemQuant {
-		max-width: 40px;
+		max-width: 80px;
 	}
 
 	#addButton {
